2efb8c48d44ccf358e37cbf9f546d04d1d524b11
Alexey Karasev <alexey.karasev@drewl.com>
2023-04-15 17:03:29 +0700

    Added tailwindcss & dasyui

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": "postcss-nesting",
+    "postcss-preset-env": { features: { "nesting-rules": false } },
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
